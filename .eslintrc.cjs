e30456070f51fef8a1c79ef7d56613c8060e8693
Ryan Wang <i@ryanc.cc>
2022-10-28 13:57:52 +0800

    chore: cleanup project files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,17 +4,17 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier"
+    "@vue/eslint-config-prettier",
   ],
   env: {
     node: true,
-    "vue/setup-compiler-macros": true
+    "vue/setup-compiler-macros": true,
   },
   rules: {
     "vue/multi-word-component-names": 0,
-    "@typescript-eslint/ban-ts-comment": 0
-  }
+    "@typescript-eslint/ban-ts-comment": 0,
+  },
 };
--- example/src/main.ts
+++ example/src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
-import "./styles/tailwind.css";
 
 const app = createApp(App);
 
