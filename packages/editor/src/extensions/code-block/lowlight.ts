8d27ae81eec5eb047daaba7e832ca2f5f62075de
Ryan Wang <i@ryanc.cc>
2022-12-15 14:57:53 +0800

    perf: HTML syntax highlighting support for code blocks

--- packages/editor/src/extensions/code-block/lowlight.ts
+++ packages/editor/src/extensions/code-block/lowlight.ts
@@ -1,3 +1,8 @@
 import { lowlight } from "lowlight";
 import "lowlight/lib/common";
+
+// https://github.com/highlightjs/highlight.js/issues/3594
+import xml from "highlight.js/lib/languages/xml";
+lowlight.registerLanguage("html", xml);
+
 export default lowlight;
