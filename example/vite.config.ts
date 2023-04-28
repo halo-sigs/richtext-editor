01bc6c4cee4793f8b1199d40e65195bed5ce0e45
gengxin <orange.geng.xin@outlook.com>
2023-04-28 20:34:18 +0800

    feat: Revert the changes made in #2 to support ImageViewResizing. (#9)
    PR from #8 
    Revert the changes made in https://github.com/halo-sigs/richtext-editor/pull/2.
    Avoid conflicts with manual size setting caused by immediate triggering after binding by checking the number of ResizeObserver events triggered.
    Remove the transition of the Image component (or at least remove the width and height) to avoid the blocking feeling caused by resizing.
    
    **This PR has changed the example-package.json,But the pack lock file was not submitted.(I have an inconsistent version of PNPM)**
    
    Fixes #8 
    
    ```release-note
    支持拖拽调整图片大小。
    ```

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import Icons from "unplugin-icons/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), Icons({ compiler: "vue3" })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
