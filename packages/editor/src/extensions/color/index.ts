5a9014e30e1a3edf8071f481a321ad018d226040
Ryan Wang <i@ryanc.cc>
2023-07-21 14:48:44 +0800

    feat: add font color setting supports
    Signed-off-by: Ryan Wang <i@ryanc.cc>

---
+++ packages/editor/src/extensions/color/index.ts
@@ -0,0 +1,46 @@
+import type { ExtensionOptions } from "@/types";
+import TiptapColor from "@tiptap/extension-color";
+import type { ColorOptions } from "@tiptap/extension-color";
+import TiptapTextStyle from "@tiptap/extension-text-style";
+import type { Editor } from "@tiptap/vue-3";
+import { markRaw } from "vue";
+import MdiFormatColor from "~icons/mdi/format-color";
+import ColorToolbarItem from "./ColorToolbarItem.vue";
+import ColorBubbleItem from "./ColorBubbleItem.vue";
+
+const Color = TiptapColor.extend<ColorOptions & ExtensionOptions>({
+  addOptions() {
+    return {
+      ...this.parent?.(),
+      getToolbarItems({ editor }: { editor: Editor }) {
+        return {
+          priority: 81,
+          component: markRaw(ColorToolbarItem),
+          props: {
+            editor,
+            isActive: false,
+            icon: markRaw(MdiFormatColor),
+            title: "字体颜色",
+          },
+        };
+      },
+      getBubbleItems({ editor }: { editor: Editor }) {
+        return {
+          priority: 51,
+          component: markRaw(ColorBubbleItem),
+          props: {
+            editor,
+            isActive: false,
+            icon: markRaw(MdiFormatColor),
+            title: "字体颜色",
+          },
+        };
+      },
+    };
+  },
+  addExtensions() {
+    return [TiptapTextStyle];
+  },
+});
+
+export default Color;
