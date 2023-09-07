36abacd217205c21440b97f2eb9d59603254b204
LIlGG <1103069291@qq.com>
2023-09-07 13:44:35 +0800

    add table bubble menu

--- packages/editor/src/extensions/table/index.ts
+++ packages/editor/src/extensions/table/index.ts
@@ -4,7 +4,7 @@ import type { NodeView } from "prosemirror-view";
 import TableCell from "./table-cell";
 import TableRow from "./table-row";
 import TableHeader from "./table-header";
-import type { Editor, Range } from "@tiptap/vue-3";
+import { isActive, type Editor, type Range } from "@tiptap/vue-3";
 import ToolbarItem from "@/components/toolbar/ToolbarItem.vue";
 import MdiTable from "~icons/mdi/table";
 import MdiTablePlus from "~icons/mdi/table-plus";
@@ -20,8 +20,9 @@ import MdiTableMergeCells from "~icons/mdi/table-merge-cells";
 import MdiTableSplitCell from "~icons/mdi/table-split-cell";
 import { markRaw } from "vue";
 import { i18n } from "@/locales";
-import ToolbarSubItem from "@/components/toolbar/ToolbarSubItem.vue";
 import type { ExtensionOptions } from "@/types";
+import type { EditorState } from "prosemirror-state";
+import { BlockActionSeparator } from "@/components";
 
 function updateColumns(
   node: ProseMirrorNode,
@@ -153,160 +154,16 @@ const Table = TiptapTable.extend<ExtensionOptions & TableOptions>({
           component: markRaw(ToolbarItem),
           props: {
             editor,
-            isActive: editor.isActive("table"),
-            icon: markRaw(MdiTable),
-            title: i18n.global.t("editor.menus.table.title"),
+            isActive: false,
+            icon: markRaw(MdiTablePlus),
+            title: i18n.global.t("editor.menus.table.add"),
+            action: () =>
+              editor
+                .chain()
+                .focus()
+                .insertTable({ rows: 3, cols: 3, withHeaderRow: true })
+                .run(),
           },
-          children: [
-            {
-              priority: 10,
-              component: markRaw(ToolbarSubItem),
-              props: {
-                editor,
-                isActive: false,
-                icon: markRaw(MdiTablePlus),
-                title: i18n.global.t("editor.menus.table.add"),
-                action: () =>
-                  editor
-                    .chain()
-                    .focus()
-                    .insertTable({ rows: 3, cols: 3, withHeaderRow: true })
-                    .run(),
-              },
-            },
-            {
-              priority: 20,
-              component: markRaw(ToolbarSubItem),
-              props: {
-                editor,
-                isActive: false,
-                disabled: !editor.isActive("table"),
-                icon: markRaw(MdiTableColumnPlusBefore),
-                title: i18n.global.t("editor.menus.table.add_column_before"),
-                action: () => editor.chain().focus().addColumnBefore().run(),
-              },
-            },
-            {
-              priority: 30,
-              component: markRaw(ToolbarSubItem),
-              props: {
-                editor,
-                isActive: false,
-                disabled: !editor.isActive("table"),
-                icon: markRaw(MdiTableColumnPlusAfter),
-                title: i18n.global.t("editor.menus.table.add_column_after"),
-                action: () => editor.chain().focus().addColumnAfter().run(),
-              },
-            },
-            {
-              priority: 40,
-              component: markRaw(ToolbarSubItem),
-              props: {
-                editor,
-                isActive: false,
-                disabled: !editor.isActive("table"),
-                icon: markRaw(MdiTableHeadersEye),
-                title: i18n.global.t("editor.menus.table.toggle_header_column"),
-                action: () => editor.chain().focus().toggleHeaderColumn().run(),
-              },
-            },
-            {
-              priority: 50,
-              component: markRaw(ToolbarSubItem),
-              props: {
-                editor,
-                isActive: false,
-                disabled: !editor.isActive("table"),
-                icon: markRaw(MdiTableColumnRemove),
-                title: i18n.global.t("editor.menus.table.delete_column"),
-                action: () => editor.chain().focus().deleteColumn().run(),
-              },
-            },
-            {
-              priority: 60,
-              component: markRaw(ToolbarSubItem),
-              props: {
-                editor,
-                isActive: false,
-                disabled: !editor.isActive("table"),
-                icon: markRaw(MdiTableRowPlusBefore),
-                title: i18n.global.t("editor.menus.table.add_row_before"),
-                action: () => editor.chain().focus().addRowBefore().run(),
-              },
-            },
-            {
-              priority: 70,
-              component: markRaw(ToolbarSubItem),
-              props: {
-                editor,
-                isActive: false,
-                disabled: !editor.isActive("table"),
-                icon: markRaw(MdiTableRowPlusAfter),
-                title: i18n.global.t("editor.menus.table.add_row_after"),
-                action: () => editor.chain().focus().addRowAfter().run(),
-              },
-            },
-            {
-              priority: 80,
-              component: markRaw(ToolbarSubItem),
-              props: {
-                editor,
-                isActive: false,
-                disabled: !editor.isActive("table"),
-                icon: markRaw(MdiTableHeadersEye),
-                title: i18n.global.t("editor.menus.table.toggle_header_row"),
-                action: () => editor.chain().focus().toggleHeaderRow().run(),
-              },
-            },
-            {
-              priority: 90,
-              component: markRaw(ToolbarSubItem),
-              props: {
-                editor,
-                isActive: false,
-                disabled: !editor.isActive("table"),
-                icon: markRaw(MdiTableRowRemove),
-                title: i18n.global.t("editor.menus.table.delete_row"),
-                action: () => editor.chain().focus().deleteRow().run(),
-              },
-            },
-            {
-              priority: 100,
-              component: markRaw(ToolbarSubItem),
-              props: {
-                editor,
-                isActive: false,
-                disabled: !editor.isActive("table"),
-                icon: markRaw(MdiTableMergeCells),
-                title: i18n.global.t("editor.menus.table.merge_cells"),
-                action: () => editor.chain().focus().mergeCells().run(),
-              },
-            },
-            {
-              priority: 110,
-              component: markRaw(ToolbarSubItem),
-              props: {
-                editor,
-                isActive: false,
-                disabled: !editor.isActive("table"),
-                icon: markRaw(MdiTableSplitCell),
-                title: i18n.global.t("editor.menus.table.split_cell"),
-                action: () => editor.chain().focus().splitCell().run(),
-              },
-            },
-            {
-              priority: 120,
-              component: markRaw(ToolbarSubItem),
-              props: {
-                editor,
-                isActive: false,
-                disabled: !editor.isActive("table"),
-                icon: markRaw(MdiTableRemove),
-                title: i18n.global.t("editor.menus.table.delete_table"),
-                action: () => editor.chain().focus().deleteTable().run(),
-              },
-            },
-          ],
         };
       },
       getCommandMenuItems() {
@@ -323,6 +180,137 @@ const Table = TiptapTable.extend<ExtensionOptions & TableOptions>({
               .insertTable({ rows: 3, cols: 3, withHeaderRow: true })
               .run();
           },
+        };
+      },
+      getBubbleMenu({ editor }) {
+        return {
+          pluginKey: "tableBubbleMenu",
+          shouldShow: ({ state }: { state: EditorState }): boolean => {
+            return isActive(state, Table.name);
+          },
+          getRenderContainer(node) {
+            let container = node;
+            if (container.nodeName === "#text") {
+              container = node.parentElement as HTMLElement;
+            }
+            while (
+              container &&
+              container.classList &&
+              !container.classList.contains("tableWrapper")
+            ) {
+              container = container.parentElement as HTMLElement;
+            }
+            return container;
+          },
+          tippyOptions: {
+            offset: [26, 0],
+          },
+          items: [
+            {
+              priority: 10,
+              props: {
+                icon: markRaw(MdiTableColumnPlusBefore),
+                title: i18n.global.t("editor.menus.table.add_column_before"),
+                action: () => editor.chain().focus().addColumnBefore().run(),
+              },
+            },
+            {
+              priority: 20,
+              props: {
+                icon: markRaw(MdiTableColumnPlusAfter),
+                title: i18n.global.t("editor.menus.table.add_column_after"),
+                action: () => editor.chain().focus().addColumnAfter().run(),
+              },
+            },
+            {
+              priority: 30,
+              props: {
+                icon: markRaw(MdiTableColumnRemove),
+                title: i18n.global.t("editor.menus.table.delete_column"),
+                action: () => editor.chain().focus().deleteColumn().run(),
+              },
+            },
+            {
+              priority: 40,
+              component: markRaw(BlockActionSeparator),
+            },
+            {
+              priority: 50,
+              props: {
+                icon: markRaw(MdiTableRowPlusBefore),
+                title: i18n.global.t("editor.menus.table.add_row_before"),
+                action: () => editor.chain().focus().addRowBefore().run(),
+              },
+            },
+            {
+              priority: 60,
+              props: {
+                icon: markRaw(MdiTableRowPlusAfter),
+                title: i18n.global.t("editor.menus.table.add_row_after"),
+                action: () => editor.chain().focus().addRowAfter().run(),
+              },
+            },
+            {
+              priority: 70,
+              props: {
+                icon: markRaw(MdiTableRowRemove),
+                title: i18n.global.t("editor.menus.table.delete_row"),
+                action: () => editor.chain().focus().deleteRow().run(),
+              },
+            },
+            {
+              priority: 80,
+              component: markRaw(BlockActionSeparator),
+            },
+            {
+              priority: 90,
+              props: {
+                icon: markRaw(MdiTableHeadersEye),
+                title: i18n.global.t("editor.menus.table.toggle_header_column"),
+                action: () => editor.chain().focus().toggleHeaderColumn().run(),
+              },
+            },
+            {
+              priority: 100,
+              props: {
+                icon: markRaw(MdiTableHeadersEye),
+                title: i18n.global.t("editor.menus.table.toggle_header_row"),
+                action: () => editor.chain().focus().toggleHeaderRow().run(),
+              },
+            },
+            {
+              priority: 110,
+              component: markRaw(BlockActionSeparator),
+            },
+            {
+              priority: 120,
+              props: {
+                icon: markRaw(MdiTableMergeCells),
+                title: i18n.global.t("editor.menus.table.merge_cells"),
+                action: () => editor.chain().focus().mergeCells().run(),
+              },
+            },
+            {
+              priority: 130,
+              props: {
+                icon: markRaw(MdiTableSplitCell),
+                title: i18n.global.t("editor.menus.table.split_cell"),
+                action: () => editor.chain().focus().splitCell().run(),
+              },
+            },
+            {
+              priority: 140,
+              component: markRaw(BlockActionSeparator),
+            },
+            {
+              priority: 150,
+              props: {
+                icon: markRaw(MdiTableRemove),
+                title: i18n.global.t("editor.menus.table.delete_table"),
+                action: () => editor.chain().focus().deleteTable().run(),
+              },
+            },
+          ],
         };
       },
     };
