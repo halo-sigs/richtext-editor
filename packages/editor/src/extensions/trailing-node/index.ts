64843eabc836e8e0de90640cbce4c5975f96620c
Takagi <mail@e.lixingyong.com>
2023-09-11 17:24:15 +0800

    Update packages/editor/src/extensions/trailing-node/index.ts
    Co-authored-by: guqing <38999863+guqing@users.noreply.github.com>

--- packages/editor/src/extensions/trailing-node/index.ts
+++ packages/editor/src/extensions/trailing-node/index.ts
@@ -18,7 +18,6 @@ function nodeEqualsType({ types, node }) {
  * - https://github.com/ueberdosis/tiptap/tree/main/demos/src/Experiments/TrailingNode
  * - https://github.com/ueberdosis/tiptap/blob/v1/packages/tiptap-extensions/src/extensions/TrailingNode.js
  * - https://github.com/remirror/remirror/blob/e0f1bec4a1e8073ce8f5500d62193e52321155b9/packages/prosemirror-trailing-node/src/trailing-node-plugin.ts
- * -
  */
 
 const TrailingNode = Extension.create({
