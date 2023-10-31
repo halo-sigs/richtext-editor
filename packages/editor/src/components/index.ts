99af1f255535a5077bd27c617f21938c05f78a58
Takagi <1103069291@qq.com>
2023-10-31 15:09:13 +0800

    pref: optimize the export  for the editor component (#62)
    #### What type of PR is this?
    
    /kind improvement
    
    #### What this PR does / why we need it:
    
    对编辑器导出内容进行优化。
    
    将 `EditorHeader`，`EditorBubbleMenu`，`EditorContent` 等进行导出。
    
    #### Does this PR introduce a user-facing change?
    ```release-note
    None
    ```

--- packages/editor/src/components/index.ts
+++ packages/editor/src/components/index.ts
@@ -1,16 +1,15 @@
 // block
-export { default as BlockActionButton } from "./block/BlockActionButton.vue";
-export { default as BlockActionInput } from "./block/BlockActionInput.vue";
-export { default as BlockActionSeparator } from "./block/BlockActionSeparator.vue";
-export { default as BlockCard } from "./block/BlockCard.vue";
+export * from "./block";
 
 // bubble
-export { default as BubbleItem } from "./bubble/BubbleItem.vue";
-export { default as NodeBubbleMenu } from "./bubble/BubbleMenu.vue";
+export * from "./bubble";
 
 // toolbar
-export { default as ToolbarItem } from "./toolbar/ToolbarItem.vue";
-export { default as ToolbarSubItem } from "./toolbar/ToolbarSubItem.vue";
+export * from "./toolbar";
 
 // toolbox
-export { default as ToolboxItem } from "./toolbox/ToolboxItem.vue";
+export * from "./toolbox";
+
+export { default as RichTextEditor } from "./Editor.vue";
+export * from "./EditorHeader.vue";
+export * from "./EditorBubbleMenu.vue";
