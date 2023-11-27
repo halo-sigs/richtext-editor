1bd7919dc7fd6332bb6bff17830aa7efda8f885d
Takagi <1103069291@qq.com>
2023-11-27 21:02:10 +0800

    fix: resolve the issue of inline block elements not fully occupying the width (#71)
    #### What type of PR is this?
    
    /kind bug
    
    #### What this PR does / why we need it:
    
    将行内块元素（iframe, video, image, audio）默认宽度设置为 100%。用以解决设置为 100% 是宽度不准确的问题。
    
    #### How to test it?
    
    新增一个行内块元素，点击设置其大小为`桌面端大小`，查看是否占满编辑器宽度。
    
    #### Which issue(s) this PR fixes:
    
    Fixes https://github.com/halo-dev/halo/issues/4868
    
    #### Does this PR introduce a user-facing change?
    ```release-note
    解决行内块元素宽度设置不准确的问题
    ```

--- packages/editor/src/extensions/iframe/IframeView.vue
+++ packages/editor/src/extensions/iframe/IframeView.vue
@@ -42,7 +42,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <node-view-wrapper as="div" class="inline-block" :class="{ 'w-full': !src }">
+  <node-view-wrapper as="div" class="inline-block w-full">
     <div
       class="inline-block overflow-hidden transition-all text-center relative h-full"
       :style="{
